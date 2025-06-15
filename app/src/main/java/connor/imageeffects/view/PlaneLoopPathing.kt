c66934e769440b3162532f8933686e9703db6eee
C-ManSharp <connortall1996@outlook.com>
2025-06-15 17:00:29 +0100

    Add template screen

---
+++ app/src/main/java/connor/imageeffects/view/PlaneLoopPathing.kt
@@ -0,0 +1,14 @@
+package connor.imageeffects.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PlaneLoopPathing(modifier: Modifier = Modifier) {
+    Column(modifier.fillMaxSize()) {
+        Text("Hello")
+    }
+}
